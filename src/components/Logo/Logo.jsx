dc5d1895be2e287a3b7c48621af95c9203c9c0af
Lolita <lolita454462@gmail.com>
2025-05-14 22:00:17 +0200

    logo link fix

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
     return (
         <Link to="/" className={s.logoBtn}>
             <svg width="104" height="16">
-                <use href="../../../public/icons/sprite.svg#icon-logo"></use>
+                <use href="/icons/sprite.svg#icon-logo"></use>
             </svg>
         </Link>
     );
